3d401b7794f7628b27d0ec81404b9eff04812102
Fernando <fernandozelaya64@hotmail.com>
2023-01-12 15:27:26 -0600

    Added movies columns assertion

--- src/components/MovieList.cy.jsx
+++ src/components/MovieList.cy.jsx
@@ -1,30 +1,33 @@
 import React from "react";
 import MovieList from "./MovieList";
 
-describe.skip("<MovieList />", () => {
+describe("<MovieList />", () => {
   beforeEach(() => {
     cy.mount(<MovieList />);
   });
 
+  //Table should be empty at first
   it("Check empty table", () => {
-    //It should first be empty
-    cy.get("[data-cy=emptyMovieList]").contains("No Movies");
+    cy.get("[data-cy=emptyMovieList]").should("exist").contains("No Movies");
     const button = cy.get("[data-cy=loadButton]");
     const moviesTable = cy.get("[data-cy=moviesTable]");
     button.should("not.be.disabled");
     moviesTable.children().should("have.length", 0);
   });
 
+  //Clicks tha Load button to get movies
   it("Check table with data", () => {
     const button = cy.get("[data-cy=loadButton]");
     const moviesTable = cy.get("[data-cy=moviesTable]");
     button.click();
     button.should("be.disabled");
-    moviesTable.children("tbody").find("tr").should("have.length", 20);
+    const moviesRows = moviesTable.children("tbody").find("tr");
+    moviesRows.should("have.length", 20);
+    moviesRows.children().should("have.length", 40);
   });
 });
 
-describe("Basic API test examples", () => {
+describe("Movies API Get", () => {
   const options = {
     method: "GET",
     url: "https://moviesdatabase.p.rapidapi.com/titles",
